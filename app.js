a7688141ca1b29647754a8238657874bb0cd7212
Mert Okumusoglu <mertokumusoglu4@gmail.com>
2023-04-13 01:59:32 +0300

    recent changes have been made

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require("express")
-const app = express()
-const port = 3000
+const ejs = require("ejs")
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" }
+const port = 3000
+const app = express()
+
+app.set("view engine", "ejs")
+app.use(express.static('public'));
+
+const path = require("path")
+
 
 app.get("/", (req, res) => {
-    res.send(blog)
+    res.render("index")
+})
+
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+
+app.get("/add_post", (req, res) => {
+    res.render("add_post")
+})
+
+app.get("post", (req, res) => {
+    res.render("post")
 })
 
 app.listen(port, () => {
