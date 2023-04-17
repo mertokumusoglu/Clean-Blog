0a8f27364253f6f03d78ba5fe80c1a9a09b12598
Mert Okumusoglu <mertokumusoglu4@gmail.com>
2023-04-17 23:36:41 +0300

    added new crud operations

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const ejs = require("ejs");
 const path = require("path");
 const mongoose = require("mongoose");
 const Blog = require("./models/Blog");
+const methodOverride = require('method-override');
+const blogController = require("./controllers/blogControllers");
+const pageController = require("./controllers/pageControllers");
 
 const port = 3000
 const app = express()
@@ -19,37 +22,23 @@ mongoose.connect("mongodb://127.0.0.1:27017/cleanblog-test-db", {
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method', {
+  methods: ["POST", "GET"]
+}));
 
-// Routes
-app.get("/", async (req, res) => {
-    const blogs = await Blog.find({})
-    res.render("index", { blogs })
-});
+// ROUTES
+app.get("/", blogController.getAllBlogs);
+app.get("/about", pageController.getAboutPage)
+app.get("/add_post", pageController.getAddPage)
+app.get("/post", pageController.getPostPage)
 
-app.get("/about", (req, res) => {
-    res.render("about")
-})
 
-app.get("/add_post", (req, res) => {
-    res.render("add_post")
-})
-
-app.get("/post", (req, res) => {
-    res.render("post")
-})
-
-app.get("/blogs/:id", async (req, res) => {
-    const blog = await Blog.findById(req.params.id)
-    res.render("post", {
-        blog
-    })
-})
-
-// create element for DB
-app.post("/blogs", async (req, res) => {
-    await Blog.create(req.body)
-    res.redirect("/")
-})
+// ROUTES - CRUD
+app.get("/blogs/:id", blogController.getBlog)
+app.post("/blogs", blogController.createBlog)
+app.get("/blogs/edit/:id", pageController.getEditPage)
+app.put("/blogs/:id", blogController.updateBlog)
+app.delete("/blogs/:id", blogController.deleteBlog)
 
 app.listen(port, () => {
     console.log("sunucu baslatildi")
